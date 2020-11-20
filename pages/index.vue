edebeef4a4b5115b3aa99c035d97156b33da1d10
Guy Levi <robozevel@gmail.com>
2020-11-20 17:39:47 +0200

    nuxt example

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    Hello {{ time }}
+  </main>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    time: null
+  }),
+  async mounted () {
+    this.time = await fetch('/api/time').then(res => res.json())
+  }
+}
+</script>
